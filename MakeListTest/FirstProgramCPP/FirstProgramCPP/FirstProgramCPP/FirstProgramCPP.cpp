3c45651797d9eaf82336a62d128ea62053034658
Chris Bagwell <49886936+CJMoneyBags@users.noreply.github.com>
2026-03-26 19:06:22 -0600

    initial

---
+++ MakeListTest/FirstProgramCPP/FirstProgramCPP/FirstProgramCPP/FirstProgramCPP.cpp
@@ -0,0 +1,14 @@
+﻿// FirstProgramCPP.cpp : Defines the entry point for the application.
+//
+
+#include "FirstProgramCPP.h"
+
+
+int main()
+{
+	std::cout << "Number1" << std::endl;
+	std::cout << "Number2" << std::endl;
+	std::cout << "Number3" << std::endl;
+	
+	return 0;
+}
